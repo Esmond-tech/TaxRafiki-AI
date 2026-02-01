34a96cb907bb141a013ff71ae873967a4ea522ac
Esmond-tech <carlosesmond18@gmail.com>
2026-02-01 18:11:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from pypdf import PdfReader
-# CHANGED: Use the stable path that works on all versions
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# THIS IS THE FIX: We use the modern import now that the requirement is added
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
